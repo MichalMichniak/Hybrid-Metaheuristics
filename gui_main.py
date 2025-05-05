5acd4d414b59c62cb174efe4210faa605bbfdadd
zojapila <80410917+zojapila@users.noreply.github.com>
2025-05-05 14:20:16 +0200

    gui ver beta test beta

---
+++ gui_main.py
@@ -0,0 +1,491 @@
+import numpy as np
+from typing import List
+import numpy as np
+from typing import List, Tuple
+from copy import deepcopy
+import random
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QCoreApplication, QMetaObject, QObject, Signal, QThread
+from PySide6.QtGui import QTextCursor
+from ui import Ui_MainWindow
+
+N = 50
+TABOO_NEIGHBORS = 30
+MIN_COST = np.inf
+LONG_TERM_CONST = 2
+W = np.random.random((N,N))
+D = np.random.random((N,N))*(np.ones((N,N)) - np.eye(N))
+D = (D+D.T)/2
+SURV_PART = 0.6
+MUTATION_PROB = 0.3
+        
+        
+class Instance:
+    def __init__(self):
+        self.perm_matrix = np.zeros((N,N), dtype = float)
+        self.best_matrix = np.zeros((N,N), dtype = float)
+        self.best_cost = np.inf
+        self.velocity_matrix = np.zeros((N,N), dtype = float)
+        self.permutation = np.zeros((N,), dtype= int)
+        self.PSO_min = np.zeros((N,N), dtype = float) # argument dla minimalnej wartości jaką osiągnęła dana instancja
+        self.taboo_lst = []
+        self.taboo_size = 10
+        self.taboo_longterm_lst = np.zeros((N,N-1)) # convention smaler first
+
+        ### PSO ###
+        self.omega = 0.8
+        self.c_1 = 2.8
+        self.c_2 = 2.8
+        self.B1 = 5
+        self.B2 = 5
+        ### END PSO ###
+
+        ### islands ###
+
+        self.histogram = np.zeros((N,), dtype=int)
+
+        ### end islands ###
+
+
+        ### GA -> PSO ###
+        self.c_1_GA_PSO = 0.5
+        self.c_2_GA_PSO = 0.5
+        pass
+    
+    def make_hist(self):
+        self.histogram = np.zeros((N,), dtype=int)
+        for i in range(len(self.taboo_lst)):
+            a,b = self.taboo_lst[i]
+            self.histogram[a] += 1
+            self.histogram[b] += 1
+            
+
+
+
+    def PSO_step(self, p_d):
+        #update pojedynczej cząstki
+        self.velocity_matrix = self.omega*self.velocity_matrix + np.random.normal(0.2)*self.c_1*(self.best_matrix - self.perm_matrix) + np.random.rand()*self.c_2*(p_d - self.perm_matrix)
+        self.perm_matrix = np.minimum(1,np.maximum(0, self.perm_matrix + self.velocity_matrix))
+        pass
+    
+    def inverse_permutation(self,perm):
+        temp = np.array(list(range(len(perm)))).reshape((1,len(perm)))
+        temp2 = perm.reshape((1,len(perm)))
+        temp3 = np.concatenate((temp2,temp),axis=0)
+        return temp3[:,np.argsort(temp3[0,:])][1,:]
+
+
+    def PSO_QAP_cost(self):
+        A = W@(self.perm_matrix.T)@D
+        sum = 0
+        for i in range(len(self.permutation)):
+            sum+=A[i][self.permutation[i]]
+        return sum
+
+    def PSO_full_QAP_cost(self):
+        A = W@(self.perm_matrix.T)@D@(self.perm_matrix)
+        
+        return np.trace(A)
+
+    def real_QAP_cost(self):
+        W_prim = np.zeros((N,N))
+        for j in range(N):
+            W_prim[:,self.permutation[j]] = W[:,j]
+            pass
+        A = W_prim@D
+        sum = 0
+        for i in range(len(self.permutation)):
+            sum+=A[i][self.permutation[i]]
+        return sum
+
+    def fuzzy_matrix_to_permutation(self):
+        idx_col = list(range(N))
+        idx_row = list(range(N))
+        self.permutation = np.zeros((N,), dtype= int)
+        
+        for i in range(N):
+            curr_max = 0
+            curr_x = 0
+            curr_y = 0
+            for j in range(len(idx_col)):
+                for k in range(len(idx_row)):
+                    if(curr_max<self.perm_matrix[idx_row[k]][idx_col[j]]):
+                        curr_max = self.perm_matrix[idx_row[k]][idx_col[j]]
+                        curr_x = j
+                        curr_y = k
+            self.permutation[idx_row[curr_y]] = idx_col[curr_x]
+            del idx_col[curr_x]
+            del idx_row[curr_y]
+    
+    def penality(self):
+        return self.B1*np.sum((np.sum(self.perm_matrix,axis=1)-1)**2) + self.B2*np.sum((np.sum(self.perm_matrix,axis=0)-1)**2)
+
+    
+
+    def PSO_to_taboo(self):
+        self.fuzzy_matrix_to_permutation()
+        for i in range(self.taboo_size):
+            ind = np.unravel_index(np.argmax(self.perm_matrix, axis=None), self.perm_matrix.shape)
+            self.perm_matrix[ind[0],ind[1]] = 0
+            idx2 = np.argmax(self.perm_matrix[ind[0]], axis=None)
+            # odtwarzanie listy taboo
+            if(ind[1]<idx2):
+                self.taboo_lst.append((ind[1],idx2))
+            elif(ind[1]>idx2):
+                self.taboo_lst.append((idx2,ind[1]))
+            # odtwarzanie długoterminowej
+            self.taboo_longterm_lst = np.zeros((N,N-1))
+            for n1 in range(N):
+                for n2 in range(N-1):
+                    self.taboo_longterm_lst[n1,n2] = LONG_TERM_CONST * min(self.velocity_matrix[n1,n2],self.velocity_matrix[n2,n1])
+        pass
+
+    def taboo_QAP_cost(self, solution=None):
+        if solution is None:
+            solution = self.permutation
+        total_cost = 0
+        for i in range(N):
+            for j in range(N):
+                total_cost += W[i][j] * D[solution[i]][solution[j]]
+        return total_cost
+
+    def taboo_step(self):
+        best_neighbor_cost = np.inf
+        for i in range(TABOO_NEIGHBORS): # częściowe przeszukanie sąsiedztwa
+            neighbor = deepcopy(self.permutation)
+            a, b = random.sample(range(1, N+1), 2) # wybór dwóch indeksów losowych elementów do zamiany
+            move = sorted((a - 1, b - 1))
+            if move not in self.taboo_lst:
+                neighbor[a - 1], neighbor[b - 1] = neighbor[b - 1], neighbor[a - 1]
+                neighbor_cost = self.taboo_QAP_cost(neighbor)
+                if neighbor_cost < best_neighbor_cost:
+                    best_neighbor_cost = neighbor_cost
+                    self.taboo_lst.append(move)
+                    self.permutation = neighbor
+                    if len(self.taboo_lst) > self.taboo_size:
+                        self.taboo_lst.pop(0)
+
+                # if len(self.taboo_lst) != 10:
+                #     print(len(self.taboo_lst))
+
+        return best_neighbor_cost
+        pass
+
+    def mutation(self):
+        a, b = random.sample(range(0, N), 2)
+        move = sorted((a, b))
+        max_it = self.taboo_size
+        it = 0
+        while(it < max_it and move in self.taboo_lst ):
+            a, b = random.sample(range(0, N), 2)
+            move = sorted((a, b))
+            it+=1
+        self.permutation[a], self.permutation[b] = self.permutation[b], self.permutation[a]
+        pass
+
+    def GA_to_PSO(self, V_mean):
+        self.velocity_matrix = self.c_1_GA_PSO*V_mean + self.c_2_GA_PSO*np.random.normal(0,1)
+        self.perm_matrix=np.zeros((N,N))
+        for i in range(N):
+            self.perm_matrix[i,self.permutation[i]] = 1
+        pass
+    
+    def transposition_to_matrix(self, transp : Tuple[int]): # Można szybciej
+        T = np.eye(N)
+        T[transp[0],transp[0]] = 0
+        T[transp[1],transp[1]] = 0
+        T[transp[1],transp[0]] = 1
+        T[transp[0],transp[1]] = 1
+        return T
+
+
+
+    def V_prev(self):
+        transformation = np.eye(N)
+        for i in list(range(len(self.taboo_lst)))[::-1]:
+            transformation = transformation@self.transposition_to_matrix(self.taboo_lst[i])
+        X=np.zeros((N,N))
+        for i in range(N):
+            X[i,self.permutation[i]] = 1
+        X_prev = X@transformation.T
+        return X-X_prev
+
+def PSO(population_lst : List[Instance], M_PSO = 5):
+    p_d = population_lst[0].best_matrix    # TODO: lepsza inicializacja najlepszego
+    best_cost = np.inf
+    for i in range(len(population_lst)):
+        population_lst[i].fuzzy_matrix_to_permutation()
+        cost1 = population_lst[i].PSO_full_QAP_cost()
+        cost2 = population_lst[i].PSO_QAP_cost()
+        cost3 = population_lst[i].real_QAP_cost()
+        
+        penality = population_lst[i].penality()
+        cost = cost3# + penality #cost1+penality #TODO: chose cost func
+        if(best_cost>cost):
+            best_cost = cost
+            p_d = deepcopy(population_lst[i].perm_matrix)
+            best_cost = cost
+    print(f"best PSO input: {best_cost}")
+
+    for it in range(M_PSO):
+        print(f"start iteration PSO{it}")
+        for i in range(len(population_lst)):
+            population_lst[i].PSO_step(p_d)
+
+        
+        for i in range(len(population_lst)):
+            population_lst[i].fuzzy_matrix_to_permutation()
+            cost1 = population_lst[i].PSO_full_QAP_cost()
+            cost2 = population_lst[i].PSO_QAP_cost()
+            cost3 = population_lst[i].real_QAP_cost()
+            
+            penality = population_lst[i].penality()
+            cost_main = cost3# + penality #cost1+penality #TODO: chose cost func
+            cost = cost3 + penality
+            if(best_cost>cost_main):
+                best_cost = cost_main
+                p_d = deepcopy(population_lst[i].perm_matrix)
+                print(f"inst: {i}, PSO {cost1}, PSO_half {cost2}, real {cost3}, cost {cost}")
+            if(population_lst[i].best_cost>cost):
+                population_lst[i].best_cost = cost
+                population_lst[i].best_matrix = deepcopy(population_lst[i].perm_matrix)
+    print("PSO -> Taboo")
+    for i in range(len(population_lst)):
+        population_lst[i].PSO_to_taboo()
+    pass
+
+def Taboo(population_lst : List[Instance], M_Taboo = 5):
+    best_global_cost = np.inf
+    for i in range(len(population_lst)):
+        instance_cost = population_lst[i].taboo_QAP_cost()
+        if instance_cost < best_global_cost:
+            best_global_cost = instance_cost
+    for it in range(M_Taboo):
+        print(f"start iteration Taboo: {it}")
+        for i in range(len(population_lst)):
+            best_local_cost = population_lst[i].taboo_step()
+            if best_local_cost < best_global_cost:
+                best_global_cost = best_local_cost
+                print(f"inst: {i}, Taboo {best_global_cost}")
+    min_cost = np.inf
+    for inst in population_lst:
+        temp = inst.taboo_QAP_cost()
+        if(temp < min_cost):
+            min_cost = temp
+    print(f"minimalny koszt po taboo: {min_cost}")
+    pass
+
+def PMX(inst1 : Instance, inst2 : Instance):
+    new = Instance()
+    p1 = inst1.permutation
+    p2 = inst2.permutation
+    offspring = np.zeros(len(p1), dtype=p1.dtype)
+    cutoff_1, cutoff_2 = np.sort(np.random.choice(np.arange(len(p1)+1), size=2, replace=False))
+    offspring[cutoff_1:cutoff_2] = p1[cutoff_1:cutoff_2]
+    for i in np.concatenate([np.arange(0,cutoff_1), np.arange(cutoff_2,len(p1))]):
+        candidate = p2[i]
+        while candidate in p1[cutoff_1:cutoff_2]: # allows for several successive mappings
+            candidate = p2[np.where(p1 == candidate)[0][0]]
+        offspring[i] = candidate
+    new.permutation = offspring
+    return new
+
+def GA(population_lst : List[Instance], idx : List[int]):
+    if len(idx) <3:
+        return [], idx
+    # count cost
+    costs = []
+    for i in idx:
+        costs.append(population_lst[i].real_QAP_cost())
+    costs = np.max(np.array(costs)) - np.array(costs)+0.001 #epsilon 
+    # selection
+    NO_SURV = int(np.ceil(float(len(idx))*SURV_PART))
+    NO_OFFSPRING = len(idx)-NO_SURV
+
+    #rulette
+    probability = costs/np.sum(costs)
+    surv = np.random.choice(len(idx), NO_SURV, replace=False, p=probability)
+    surv = np.array([idx[i] for i in surv])
+
+    offsprings = []
+    # offspring
+    for i in range(NO_OFFSPRING):
+        t = np.random.choice(len(idx), 2, replace=False, p=probability)
+        offsprings.append(PMX(population_lst[t[0]], population_lst[t[1]]))
+
+    # mutation of survivors
+    for i in surv:
+        if random.random() < MUTATION_PROB:
+            population_lst[i].mutation()
+
+    return offsprings,surv
+    pass
+
+class Island:
+    def __init__(self, population_lst : List[Instance], idx : List[int]):
+        self.idx : List[int] = idx # index of instances of that island
+        self.population_lst : List[Instance] = population_lst
+
+    def count_mean_transformation(self):
+        self.V_mean = np.zeros((N,N), dtype=float)
+        for i in range(len(self.idx)):
+            self.V_mean = self.V_mean + self.population_lst[self.idx[i]].V_prev()
+        self.V_mean = self.V_mean/len(self.idx)
+        pass
+
+    def run(self):
+        # wykonanie GA i zamiana na formę PSO
+        self.offsprings, self.survivors = GA(self.population_lst, self.idx)# both : List[Instance]
+        # (tu mogą być problemy z multiprocessing)
+        print("GA->PSO")
+        for i in self.survivors:
+            self.population_lst[i].GA_to_PSO(self.V_mean)
+        
+        for i in range(len(self.offsprings)):
+            self.offsprings[i].GA_to_PSO(self.V_mean)
+        self.population_lst.extend(self.offsprings)
+        self.del_idx = [idx for idx in self.idx if idx not in self.survivors]
+        return self.del_idx
+
+
+def initialization(M_start = 100) -> List[Instance]:
+    instance_lst = [Instance() for i in range(M_start)]
+    for i in range(len(instance_lst)):
+        instance_lst[i].perm_matrix = np.random.rand(N,N)
+        instance_lst[i].velocity_matrix = np.random.rand(N,N)
+    return instance_lst
+
+def simmilarity_inst(inst1 : Instance, inst2 : Instance):
+    return np.sum(np.minimum(inst1.histogram, inst2.histogram))
+
+
+def split_population(population_lst : List[Instance], M_species = 3) -> List[List[int]]:
+    
+    for i in range(len(population_lst)):
+        population_lst[i].make_hist()
+    
+    frontiers = []
+    first = population_lst[0]
+    second = population_lst[0]
+    curr_smalest = simmilarity_inst(first,second)
+    ## first two
+    for i in range(len(population_lst)):
+        for j in range(len(population_lst)):
+            if simmilarity_inst(population_lst[i], population_lst[j]) < curr_smalest:
+                curr_smalest = simmilarity_inst(population_lst[i], population_lst[j])
+                first = population_lst[i]
+                second = population_lst[j]
+    frontiers.append(first)
+    frontiers.append(second)
+
+
+    for time in range(M_species-2):
+        temp = population_lst[0]
+        curr_smalest = np.sum(np.array([simmilarity_inst(population_lst[0], inst) for inst in frontiers]))
+        for i in range(len(population_lst)):
+            temp_sim = np.sum(np.array([simmilarity_inst(population_lst[i], inst) for inst in frontiers]))
+            if temp_sim < curr_smalest:
+                curr_smalest = temp_sim
+                temp = population_lst[i]
+        frontiers.append(temp)
+    
+    idx_lst = []
+    for i in range(M_species):
+        idx_lst.append([])
+    
+    for i in range(len(population_lst)):
+        temp_idx = np.argmax(np.array([simmilarity_inst(population_lst[i], inst) for inst in frontiers]))
+        idx_lst[temp_idx].append(i)
+    print([len(idx_lst[i]) for i in range(len(idx_lst))])
+    return idx_lst#[list(range(len(population_lst)))[0:len(population_lst)//3], list(range(len(population_lst)))[len(population_lst)//3:(2*len(population_lst))//3], list(range(len(population_lst)))[(2*len(population_lst))//3:]]
+
+def run(M_PSO = 50, M_TABOO = 20, M_species = 3, M_start=100, max_it = 10):
+    population_lst : List[Instance] = initialization(M_start=M_start)
+    for i in range(max_it):
+        PSO(population_lst, M_PSO)
+        Taboo(population_lst, M_TABOO)
+
+        split_idx : List[List[int]] = split_population(population_lst, M_species )
+        del_list = []
+        for i in range(M_species):
+            island = Island(population_lst, split_idx[i])
+            island.count_mean_transformation()
+            del_list.extend(island.run())
+        del_list.sort(reverse=True)
+        for i in del_list:
+            population_lst.pop(i)
+        
+    print("finished :)")
+
+class Worker(QObject):
+    finished = Signal()
+
+    def runn(self):
+        # Tutaj umieść swoją długotrwałą operację
+        print("Rozpoczynam działanie funkcji run()")
+        run(max_it=1)
+        print("Zakończono działanie funkcji run()")
+        self.finished.emit()
+
+class EmittingStream(QObject):
+    text_written = Signal(str)
+
+    def __init__(self, original_stream):
+        super().__init__()
+        self.original_stream = original_stream
+
+    def write(self, text):
+        self.original_stream.write(text)
+        self.text_written.emit(str(text))
+
+    def flush(self):
+        self.original_stream.flush()
+    
+class MyMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        # self.text_browser = QTextBrowser(self)
+        # self.setCentralWidget(self.textBrowser)
+
+        # Przekierowanie stdout i stderr
+        self.original_stdout = sys.stdout
+        self.original_stderr = sys.stderr
+
+        # Przekieruj stdout i stderr
+        sys.stdout = EmittingStream(self.original_stdout)
+        sys.stderr = EmittingStream(self.original_stderr)
+        sys.stdout.text_written.connect(self.append_text)
+        sys.stderr.text_written.connect(self.append_text)
+        
+        self.pushButton.clicked.connect(self.start_thread)
+    
+    def append_text(self, text):
+        self.textBrowser.moveCursor(QTextCursor.MoveOperation.End)
+        self.textBrowser.insertPlainText(text)
+        self.textBrowser.ensureCursorVisible()
+            
+    def start_thread(self):
+        self.thread = QThread()
+        self.worker = Worker()
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.runn)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.thread.start()
+        
+    # def handle_run(self):
+    #     result = run(max_it=1)
+    #     # self.textBrowser.setPlainText("dupa")
+
+    
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = MyMainWindow()
+    mainWindow.show()
+    sys.exit(app.exec())
+    # run()
+    # pass
---
+++ ui.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'untitled.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
+    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
+    QTextBrowser, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(500, 80, 93, 28))
+        self.N_numer = QLineEdit(self.centralwidget)
+        self.N_numer.setObjectName(u"N_numer")
+        self.N_numer.setGeometry(QRect(170, 40, 113, 22))
+        self.N = QLabel(self.centralwidget)
+        self.N.setObjectName(u"N")
+        self.N.setGeometry(QRect(54, 40, 71, 21))
+        self.Taboo_neighbours = QLabel(self.centralwidget)
+        self.Taboo_neighbours.setObjectName(u"Taboo_neighbours")
+        self.Taboo_neighbours.setGeometry(QRect(30, 80, 111, 16))
+        self.surv_part = QLabel(self.centralwidget)
+        self.surv_part.setObjectName(u"surv_part")
+        self.surv_part.setGeometry(QRect(30, 120, 55, 16))
+        self.mut_prob = QLabel(self.centralwidget)
+        self.mut_prob.setObjectName(u"mut_prob")
+        self.mut_prob.setGeometry(QRect(30, 150, 55, 16))
+        self.M_PSO = QLabel(self.centralwidget)
+        self.M_PSO.setObjectName(u"M_PSO")
+        self.M_PSO.setGeometry(QRect(30, 180, 55, 16))
+        self.M_taboo = QLabel(self.centralwidget)
+        self.M_taboo.setObjectName(u"M_taboo")
+        self.M_taboo.setGeometry(QRect(30, 210, 55, 16))
+        self.M_start = QLabel(self.centralwidget)
+        self.M_start.setObjectName(u"M_start")
+        self.M_start.setGeometry(QRect(30, 240, 55, 16))
+        self.m_species = QLabel(self.centralwidget)
+        self.m_species.setObjectName(u"m_species")
+        self.m_species.setGeometry(QRect(30, 280, 55, 16))
+        self.max_iter = QLabel(self.centralwidget)
+        self.max_iter.setObjectName(u"max_iter")
+        self.max_iter.setGeometry(QRect(30, 320, 55, 16))
+        self.taboo_neighbours_num = QLineEdit(self.centralwidget)
+        self.taboo_neighbours_num.setObjectName(u"taboo_neighbours_num")
+        self.taboo_neighbours_num.setGeometry(QRect(170, 80, 113, 22))
+        self.surr_part_num = QLineEdit(self.centralwidget)
+        self.surr_part_num.setObjectName(u"surr_part_num")
+        self.surr_part_num.setGeometry(QRect(170, 120, 113, 22))
+        self.mut_prob_num = QLineEdit(self.centralwidget)
+        self.mut_prob_num.setObjectName(u"mut_prob_num")
+        self.mut_prob_num.setGeometry(QRect(170, 150, 113, 22))
+        self.m_pso_num = QLineEdit(self.centralwidget)
+        self.m_pso_num.setObjectName(u"m_pso_num")
+        self.m_pso_num.setGeometry(QRect(170, 180, 113, 22))
+        self.m_taboo_num = QLineEdit(self.centralwidget)
+        self.m_taboo_num.setObjectName(u"m_taboo_num")
+        self.m_taboo_num.setGeometry(QRect(170, 210, 113, 22))
+        self.m_start_num = QLineEdit(self.centralwidget)
+        self.m_start_num.setObjectName(u"lineEdit")
+        self.m_start_num.setGeometry(QRect(170, 240, 113, 22))
+        self.m_species_num = QLineEdit(self.centralwidget)
+        self.m_species_num.setObjectName(u"m_species_num")
+        self.m_species_num.setGeometry(QRect(170, 270, 113, 22))
+        self.max_iter_num = QLineEdit(self.centralwidget)
+        self.max_iter_num.setObjectName(u"max_iter_num")
+        self.max_iter_num.setGeometry(QRect(170, 320, 113, 22))
+        self.textBrowser = QTextBrowser(self.centralwidget)
+        self.textBrowser.setObjectName(u"textBrowser")
+        self.textBrowser.setGeometry(QRect(370, 140, 341, 291))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 21))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"HYBRID METAHEURISTICS", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"START", None))
+        self.N_numer.setText(QCoreApplication.translate("MainWindow", u"50", None))
+        self.N.setText(QCoreApplication.translate("MainWindow", u"N ", None))
+        self.Taboo_neighbours.setText(QCoreApplication.translate("MainWindow", u"Taboo_neighbours", None))
+        self.surv_part.setText(QCoreApplication.translate("MainWindow", u"surv_part", None))
+        self.mut_prob.setText(QCoreApplication.translate("MainWindow", u"mut_prob", None))
+        self.M_PSO.setText(QCoreApplication.translate("MainWindow", u"M_PSO", None))
+        self.M_taboo.setText(QCoreApplication.translate("MainWindow", u"M_Taboo", None))
+        self.M_start.setText(QCoreApplication.translate("MainWindow", u"M_start", None))
+        self.m_species.setText(QCoreApplication.translate("MainWindow", u"M_species", None))
+        self.max_iter.setText(QCoreApplication.translate("MainWindow", u"max_iter", None))
+        self.taboo_neighbours_num.setText(QCoreApplication.translate("MainWindow", u"30", None))
+        self.surr_part_num.setText(QCoreApplication.translate("MainWindow", u"0.6", None))
+        self.mut_prob_num.setText(QCoreApplication.translate("MainWindow", u"0.3", None))
+        self.m_pso_num.setText(QCoreApplication.translate("MainWindow", u"50", None))
+        self.m_taboo_num.setText(QCoreApplication.translate("MainWindow", u"20", None))
+        self.m_start_num.setText(QCoreApplication.translate("MainWindow", u"100", None))
+        self.m_species_num.setText(QCoreApplication.translate("MainWindow", u"3", None))
+        self.max_iter_num.setText(QCoreApplication.translate("MainWindow", u"10", None))
+    # retranslateUi
+
